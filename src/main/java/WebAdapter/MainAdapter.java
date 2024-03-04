a3979302653db91da9d8602c1710c66210aa755c
Riky47 <rich.balducchi@gmail.com>
2024-03-04 11:38:51 +0100

    Added basics of webapp version

--- src/main/java/WebAdapter/MainAdapter.java
+++ src/main/java/WebAdapter/MainAdapter.java
@@ -1,6 +1,5 @@
 package WebAdapter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
